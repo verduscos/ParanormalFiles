2eb1e95817c6cfecd1922d568bf2746a1e958f56
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-02-15 19:15:56 -0800

    created store for sighting and added reducer to rootReducer

---
+++ react-app/src/store/sighting.js
@@ -0,0 +1,30 @@
+const GET_SIGHTING = "session/GET_SIGHTING";
+
+const getSightings = (sightings) => ({
+    type: GET_SIGHTING,
+    payload: sightings
+})
+
+export const getAllSightings = () => async (dispatch) => {
+    const response = await fetch(`/api/sightings/`);
+
+    if (response.status >= 400) {
+        throw response
+    }
+
+    const data = await response.json();
+    dispatch(getSightings(data.sightings));
+    return data;
+}
+
+
+const sightingReducer = (state = {}, action) => {
+    switch (action.type) {
+        case GET_SIGHTING:
+            return { ...state, sightings: action.sightings }
+        default:
+            return state
+    }
+};
+
+export default sightingReducer;
