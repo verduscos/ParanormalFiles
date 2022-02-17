30b9fce20695413395d3ee692f3fb8cb8128062c
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-02-16 16:19:56 -0800

    created api route for fetching all comments on a specific sighting. tested on postman.

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -1,2 +1,15 @@
 from flask import Blueprint, session, request
-from app.forms
+from app.forms import CommentForm
+from app.models import Comment, db
+
+comment_routes = Blueprint("comments", __name__)
+
+
+@comment_routes.route("/<int:sightingId>")
+def get_comments(sightingId):
+    """
+    Get all comments for a specific sighting.
+    """
+    comments = Comment.query.filter(Comment.sighting_id == sightingId).all()
+
+    return {"comments": [comment.to_dict() for comment in comments]}
