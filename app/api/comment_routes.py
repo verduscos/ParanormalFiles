e4b32b19b9beb761d40edc9e6ad6fee5e0d01b74
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-02-16 16:35:25 -0800

    created api route for posting comments, tested on postman

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -5,6 +5,27 @@ from app.models import Comment, db
 comment_routes = Blueprint("comments", __name__)
 
 
+@comment_routes.route("/<int:sightingId>", methods=["POST"])
+def create_comment(sightingId):
+    """
+    Create a comment for a specific sighting.
+    """
+    form = CommentForm()
+    print("SOMETHINGGGGGGG")
+    form["csrf_token"].data = request.cookies["csrf_token"]
+    if form.validate_on_submit():
+        comment = Comment(
+            user_id=request.json["user_id"],
+            sighting_id=sightingId,
+            comment=request.json["comment"]
+        )
+        db.session.add(comment)
+        db.session.commit()
+
+        return {"comment": comment.to_dict()}
+    return {"errors": form.errors}, 400
+
+
 @comment_routes.route("/<int:sightingId>")
 def get_comments(sightingId):
     """
