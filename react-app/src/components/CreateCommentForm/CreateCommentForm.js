7b2596fc69e6edd2755dec741df567879043d63d
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-02-17 10:45:17 -0800

    create component for comments working, but  comments from prev sighting display on a newly created sighting without a hard refresh. will debug after update and delete

--- react-app/src/components/CreateCommentForm/CreateCommentForm.js
+++ react-app/src/components/CreateCommentForm/CreateCommentForm.js
@@ -1,13 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { createAComment } from "../../store/comment";
+
 
 
 const CreateCommentForm = () => {
+  const params = useParams();
+  let currentUser = useSelector(state => state.session.user)
+  const { sightingId } = params;
+  const dispatch = useDispatch();
+  const [comment, setComment] = useState("");
 
+  console.log(comment)
+
+  const createComment = (e) => {
+    e.preventDefault();
+
+    const payload = {
+      user_id: currentUser.id,
+      sighting_id: sightingId,
+      comment: comment
+    }
+
+    console.log(payload);
+
+    dispatch(createAComment(payload))
+    console.log("INSIDE BTN")
+  }
 
   return (
     <>
-      <form>
-        <textarea></textarea>
+      <form onSubmit={createComment}>
+        <textarea
+        value={comment}
+        onChange={(e) => {
+          setComment(e.target.value)
+        }}
+        ></textarea>
         <button>CANCEL</button>
         <button>COMMENT</button>
       </form>
--- react-app/src/store/comment.js
+++ react-app/src/store/comment.js
@@ -1,8 +1,15 @@
 const GET_COMMENTS = "session/GET_COMMENTS"
+const CREATE_COMMENT = "session/CREATE_COMMENT"
+
 
 const getComments = (comments) => ({
   type: GET_COMMENTS,
   payload: comments
+})
+
+const createComment = (comment) => ({
+  type: CREATE_COMMENT,
+  payload: comment
 })
 
 
@@ -19,11 +26,47 @@ export const getALLComments = (sightingId) => async (dispatch) => {
 
 }
 
+export const createAComment = (payload) => async (dispatch) => {
+  const response = await fetch(`/api/comments/${payload.sighting_id}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      user_id: payload.user_id,
+      sighting_id: payload.sighting_id,
+      comment: payload.comment
+    })
+  })
+
+  if (response.status >= 400) {
+    throw response;
+  }
+
+  if (response.ok) {
+    const data = await response.json();
+    console.log(data)
+    dispatch(createComment(data))
+
+    return data;
+  }
+}
+
 
 const commentsReducer = (state = {}, action) => {
   switch (action.type) {
     case GET_COMMENTS:
       return { ...state, ...action.payload.comments }
+    case CREATE_COMMENT:
+      let comments = {}
+      console.log("INSIDE REDUCER")
+      console.log(action.payload)
+      comments[action.payload.comment.id] = action.payload.comment
+
+      console.log(comments)
+
+      return { ...state, ...comments}
+      // return { ...state}
     default:
       return state;
   }
