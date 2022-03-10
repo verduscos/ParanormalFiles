4f3a9ae61870addd989cdb449b149519219828b2
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-03-10 11:04:11 -0800

    added no search results h3, added search results for h2

--- react-app/src/components/Search/Search.js
+++ react-app/src/components/Search/Search.js
@@ -20,43 +20,49 @@ const Search = () => {
     <div id="sightings-container">
       <div id="sightings-inner">
 
-    <h1>Results for: {string}</h1>
-    {sightingsArray.map((sighting, i) => (
-      <ul id="sighting-card" key={sighting?.id}>
-            {/* <li key={sighting.id}>{sighting.location}</li> */}
-            <div>
-              <li className="card-r1" key={`date-${sighting?.id}`}>
-                <p>{sighting?.username}</p>
-                {/* TODO
+        <h1 className="search-headers">Search Results for: {string}</h1>
+        {sightingsArray.length ?
+        <>
+          {
+            sightingsArray.map((sighting, i) => (
+              <ul id="sighting-card" key={sighting?.id}>
+                {/* <li key={sighting.id}>{sighting.location}</li> */}
+                <div>
+                  <li className="card-r1" key={`date-${sighting?.id}`}>
+                    <p>{sighting?.username}</p>
+                    {/* TODO
                   FIX DATE FORMAT
                 */}
-                <p>{sighting?.date}</p>
-              </li>
-              <Link className="link" to={`/sightings/${sighting?.id}`} key={`link-${i}`}>
-                <div key={`title-${sighting?.id}`}>
-                  <h2 className="card-text">{sighting.title}</h2>
-                  <p className="card-text card-story">{sighting.description}</p>
+                    <p>{sighting?.date}</p>
+                  </li>
+                  <Link className="link" to={`/sightings/${sighting?.id}`} key={`link-${i}`}>
+                    <div key={`title-${sighting?.id}`}>
+                      <h2 className="card-text">{sighting.title}</h2>
+                      <p className="card-text card-story">{sighting.description}</p>
+                    </div>
+                  </Link>
+
+
+                  <div id="sighting-date">
+                    <p>{`${sighting?.created_at.split(' ')[2]} ${sighting.created_at.split(' ')[1]}, ${sighting.created_at.split(' ')[3]} in `}</p>
+                    <Link className="link" to={`/sightings/categories/${sighting?.category}`}>
+                      <li className="category-link" key={`category-${sighting?.id}`} >{sighting?.category}</li>
+                    </Link>
+                  </div>
+
                 </div>
-              </Link>
-
-
-              <div id="sighting-date">
-                <p>{`${sighting?.created_at.split(' ')[2]} ${sighting.created_at.split(' ')[1]}, ${sighting.created_at.split(' ')[3]} in `}</p>
-                <Link className="link" to={`/sightings/categories/${sighting?.category}`}>
-                  <li className="category-link" key={`category-${sighting?.id}`} >{sighting?.category}</li>
+                <Link className="link card-img" to={`/sightings/${sighting?.id}`} key={`link-${i}-img`}>
+                  <img className="card-img" src={sighting?.image_url} alt="sighting-img"></img>
                 </Link>
-              </div>
+              </ul>
+            ))
+          }
 
-            </div>
-            <Link className="link card-img" to={`/sightings/${sighting?.id}`} key={`link-${i}-img`}>
-              <img className="card-img" src={sighting?.image_url} alt="sighting-img"></img>
-            </Link>
-          </ul>
-        ))}
+            </>
 
 
-
-        </div>
+          : <h3 className="search-headers">Sorry, but nothing matched your search terms.</h3>}
+      </div>
     </div>
   )
 }
