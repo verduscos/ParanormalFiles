003d8bd4db7d13f239d649d506a2957491fe3c98
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-05-31 12:30:04 -0700

    added click event to tags, queries for that topic

--- react-app/src/components/Tags/index.js
+++ react-app/src/components/Tags/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import * as sessions from "../../store/sighting"
 import './Tags.css';
 
 const Tags = () => {
-  const categories = ["UFOs", "Ghosts", "Demons", "Angels", "Reincarnation", "Monsters", "Mandela Effect", "Time Travel", 'Synchronicity'];
+  const dispatch = useDispatch();
+  const history = useHistory();
 
+  const categories = ["UFOs", "Angels", "Reincarnation", "Ghosts", "Monsters", "Mandela Effect", "Time Travel", "Demons", 'Synchronicity'];
+
+  const search = async (e, searchStr) => {
+    e.preventDefault();
+
+    dispatch(sessions.searchAllSightings(searchStr))
+    history.push(`/sightings/search/${searchStr}`)
+  }
 
   return (
     <div id="tags-container">
       <h2 id="tags-header">DISCOVER MORE OF WHAT MATTERS TO YOU</h2>
 
       <ul id="categories-list">
-        {categories.map( category => (
-          <li>{category}</li>
+        {categories.map(category => (
+          <li
+            onClick={(e) => search(e, category)}
+          >{category}</li>
         ))}
       </ul>
     </div>
