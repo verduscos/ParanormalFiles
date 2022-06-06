2608c0a0666e97ec6ce2ff9b39900c3b671ac0df
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-06-06 11:05:56 -0700

    moved css for guest nav to GuestNav.css

--- react-app/src/components/Navigation/SpashBg.js
+++ react-app/src/components/Navigation/SpashBg.js
@@ -13,12 +13,12 @@ const SplashBg = () => {
       {/* LOGO FOR MOBILE */}
       <div id="splash-logo-container">
         <img id="splash-logo" src={logo} alt="pf-logo" />
+        <LoginFormModal />
       </div>
       <nav id="splash-guest">
         <ul id="splash-ul">
           <h1 id="splash-header">Paranormal Files</h1>
           <h2 id="splash-subheader">Discuss and share paranormal experiences.</h2>
-          <LoginFormModal />
           <SignupFormModal />
         </ul>
       </nav>
