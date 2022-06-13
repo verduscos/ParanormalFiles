27500fe9fe6d616e6603053ea3bc939f5310a42b
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-06-12 22:17:08 -0700

    updated sighting tags to display on desktop view

--- react-app/src/components/sightings/Sightings.js
+++ react-app/src/components/sightings/Sightings.js
@@ -39,19 +39,14 @@ const Sightings = () => {
                 </div>
               </Link>
             </li>
-            <li>
+            <li id="sighting-tag-container">
               <span id="sighting-date">{`${sighting?.created_at.split(' ')[2]} ${sighting.created_at.split(' ')[1]}`}</span>
               {/* , ${sighting.created_at.split(' ')[3]} */}
 
-              {/* <Link className="link" to={`/sightings/categories/${sighting?.category}`}>
+              <Link className="link tag" to={`/sightings/categories/${sighting?.category}`}>
                   <p className="category-link" key={`category-${sighting?.id}`} >{sighting?.category}</p>
-                </Link> */}
-            </li>
-            {/* <li>
-              <Link className="link" to={`/sightings/${sighting?.id}`} key={`link-${i}-img`}>
-                <img className="sighting-img" src={sighting?.image_url} alt="sighting-img"></img>
                 </Link>
-            </li> */}
+            </li>
           </ul>
           <Link className="link" to={`/sightings/${sighting?.id}`} key={`link-${i}-img`}>
             <img className="sighting-img" src={sighting?.image_url} alt="sighting-img"></img>
