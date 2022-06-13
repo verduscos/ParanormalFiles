ab4e5ce8f02610387b50a6ddf1c7cd6e60a7a86a
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-06-13 16:50:19 -0700

    updated nav border for desktop, switched to a useState variable instead of a hardcoded nav col-2 id

--- react-app/src/components/Main/index.js
+++ react-app/src/components/Main/index.js
@@ -10,11 +10,13 @@ const Main = () => {
   const dispatch = useDispatch()
   let currentUser = useSelector(state => state.session.user)
   const [navId, setNavId] = useState("col-1-guest");
-  const [sightingId, setSightingId] = useState("col-3-guest");
+  const [sightingsId, setSightingsId] = useState("col-3-guest");
+  const [tagsId, setTagsId] = useState("col-2-guest");
 
   useEffect(() => {
     currentUser !== null ? setNavId("col-1-user") : setNavId("col-1-guest");
-    currentUser !== null ? setSightingId("col-3-user") : setSightingId("col-3-guest");
+    currentUser !== null ? setSightingsId("col-3-user") : setSightingsId("col-3-guest");
+    currentUser !== null ? setTagsId("col-2-user") : setTagsId("col-2-guest");
   }, [currentUser])
 
   return (
@@ -22,11 +24,11 @@ const Main = () => {
       <div id={navId}>
         <Navigation />
       </div>
-      <div id="col-2">
+      <div id="col-2-user">
         <Tags />
         <Footer/>
       </div>
-      <div id={sightingId}>
+      <div id={sightingsId}>
         <Sightings />
       </div>
     </div>
