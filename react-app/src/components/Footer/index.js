6e3b001be99f54628d9465357ce0b48429c30394
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-06-14 12:25:07 -0700

    updated footer react icons, created a mobile and desktop footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { VscGithub } from "react-icons/vsc"
-import { BsLinkedin } from "react-icons/bs"
+import { BsLinkedin } from "react-icons/bs";
+import { IoLogoGithub } from "react-icons/io";
+import { FaUserCircle } from "react-icons/fa";
 import "./Footer.css"
 
 const Footer = () => {
@@ -8,12 +9,15 @@ const Footer = () => {
 
   return (
     <footer id="footer-container">
-      <ul id="test8">
+      <ul>
         <li>
           <p>Developed by Eddie Verdusco</p>
         </li>
         <li className='social-link' key="github-link">
-          <a href={"https://github.com/verduscos"} target="_blank" rel="noreferrer"><VscGithub /></a>
+          <a href={"https://github.com/verduscos"} target="_blank" rel="noreferrer"><IoLogoGithub /></a>
+        </li>
+        <li className='social-link' key="portfolio-link">
+        <a href={"https://verduscos.github.io/portfolio/"} target="_blank" rel="noreferrer"><FaUserCircle /></a>
         </li>
         <li className='social-link' key="linkedin-link">
           <a href={"https://www.linkedin.com/in/eddie-verdusco/"} target="_blank" rel="noreferrer"><BsLinkedin /></a>
@@ -22,6 +26,5 @@ const Footer = () => {
     </footer>
   )
 }
-
 
 export default Footer;
