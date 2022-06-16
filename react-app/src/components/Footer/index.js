6710f92a14825d84cec720b67721b6cd1cca9725
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-06-15 19:22:34 -0700

    updated mobile footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -10,18 +10,18 @@ const Footer = () => {
   return (
     <footer id="footer-container">
       <ul>
-        <li>
+        <li id="footer-dev">
           <p>Developed by Eddie Verdusco</p>
         </li>
         <li className='social-link' key="github-link">
           <a href={"https://github.com/verduscos"} target="_blank" rel="noreferrer"><IoLogoGithub /></a>
-        </li>
-        <li className='social-link' key="portfolio-link">
-        <a href={"https://verduscos.github.io/portfolio/"} target="_blank" rel="noreferrer"><FaUserCircle /></a>
-        </li>
-        <li className='social-link' key="linkedin-link">
+          <a href={"https://verduscos.github.io/portfolio/"} target="_blank" rel="noreferrer"><FaUserCircle /></a>
           <a href={"https://www.linkedin.com/in/eddie-verdusco/"} target="_blank" rel="noreferrer"><BsLinkedin /></a>
         </li>
+        {/* <li className='social-link' key="portfolio-link">
+        </li>
+        <li className='social-link' key="linkedin-link">
+        </li> */}
       </ul>
     </footer>
   )
