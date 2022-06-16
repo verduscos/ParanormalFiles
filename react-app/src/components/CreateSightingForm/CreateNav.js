725b498c56ad73dbf46f8cdcd49ca2d35ab4de3e
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-06-15 23:05:15 -0700

    updated nav for reporting a sighitng

--- react-app/src/components/CreateSightingForm/CreateNav.js
+++ react-app/src/components/CreateSightingForm/CreateNav.js
@@ -2,8 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from 'react-router-dom';
 import ProfileButton from "../Navigation/ProfileButton";
-// import logo from "../Navigation/logo-paranormal-files.png"
-// import Logo from '../Navigation/pf-log.png';
+import Logo from '../Navigation/pf-logo.png';
 
 
 const CreateNav = () => {
@@ -13,11 +12,10 @@ const CreateNav = () => {
   return (
     <nav id="create-nav">
       <ul>
-        <div id="create-nav-link">
-          <NavLink to="/"><img alt="paranormal-files-logo" /></NavLink>
+        <li id="create-nav-home-link">
+          <NavLink to="/"><img id="pf-logo" src={Logo} alt="pf-logo" /></NavLink>
           <span>Draft in {currentUser.username}</span>
-        </div>
-        {/* <ProfileButton /> */}
+        </li>
         <li className="user-nav-btn">
           <ProfileButton user={currentUser} />
         </li>
