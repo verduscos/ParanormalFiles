4464ce26418e47d750b10d4bc8883d874c3202df
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-06-18 19:54:58 -0700

    updated useEffect for Sightings

--- react-app/src/components/sightings/Sightings.js
+++ react-app/src/components/sightings/Sightings.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useLocation } from 'react-router-dom'
 import * as sessionActions from "../../store/sighting"
@@ -6,6 +6,7 @@ import "./sightings.css"
 
 const Sightings = () => {
   const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true);
   let currentUser = useSelector(state => state.session.user)
   let sightings = useSelector(state => state.sightings);
   let sightingsArray = Object.values(sightings);
@@ -27,50 +28,57 @@ const Sightings = () => {
     } else if (path === "/favorites") {
       dispatch(sessionActions.getAllFavorites(currentUser.id));
     } else {
-      dispatch(sessionActions.getAllUserSightings());
+      dispatch(sessionActions.getAllUserSightings(currentUser.id));
     }
   }, [path])
 
+  const loadingIcon = (
+    <h1>LOADING</h1>
+  )
 
   return (
-    <div id="sightings-container">
-      {!sightingsArray.length ?
-        <>
-          <h2>No results found.</h2>
-        </>
-        : null}
 
-      {sightingsArray.map((sighting, i) => (
-        <div id="sighting-card">
-          <ul id="sighting-details" key={sighting?.id}>
-            <li key={`date-${sighting?.id}`}>
-              <h4 id="sighting-author">
-                {sighting?.username}
-              </h4>
-            </li>
-            <li>
-              <Link className="link" to={`/sightings/${sighting?.id}`} key={`link-${i}`}>
-                <div key={`title-${sighting?.id}`}>
-                  <h2 id="sighting-title">{sighting.title}</h2>
-                  <p className="sighting-story">{sighting.description}</p>
-                </div>
-              </Link>
-            </li>
-            <li id="sighting-tag-container">
-              <span id="sighting-date">{`${sighting?.created_at.split(' ')[2]} ${sighting.created_at.split(' ')[1]}`}</span>
-              {/* , ${sighting.created_at.split(' ')[3]} */}
+<div id="sightings-container">
+  {!sightingsArray.length ?
+    <>
+      <h2>No results found.</h2>
+    </>
+    : null}
 
-              <Link className="link tag" to={`/sightings/categories/${sighting?.category}`}>
-                <p className="category-link" key={`category-${sighting?.id}`} >{sighting?.category}</p>
-              </Link>
-            </li>
-          </ul>
-          <Link className="link" to={`/sightings/${sighting?.id}`} key={`link-${i}-img`}>
-            <img className="sighting-img" src={sighting?.image_url} alt="sighting-img"></img>
+  {sightingsArray.map((sighting, i) => (
+    <div id="sighting-card">
+      <ul id="sighting-details" key={sighting?.id}>
+        <li key={`date-${sighting?.id}`}>
+          <h4 id="sighting-author">
+            {sighting?.username}
+          </h4>
+        </li>
+        <li>
+          <Link className="link" to={`/sightings/${sighting?.id}`} key={`link-${i}`}>
+            <div key={`title-${sighting?.id}`}>
+              <h2 id="sighting-title">{sighting.title}</h2>
+              <p className="sighting-story">{sighting.description}</p>
+            </div>
           </Link>
-        </div>
-      ))}
+        </li>
+        <li id="sighting-tag-container">
+          <span id="sighting-date">{`${sighting?.created_at.split(' ')[2]} ${sighting.created_at.split(' ')[1]}`}</span>
+          {/* , ${sighting.created_at.split(' ')[3]} */}
+
+          <Link className="link tag" to={`/sightings/categories/${sighting?.category}`}>
+            <p className="category-link" key={`category-${sighting?.id}`} >{sighting?.category}</p>
+          </Link>
+        </li>
+      </ul>
+      <Link className="link" to={`/sightings/${sighting?.id}`} key={`link-${i}-img`}>
+        <img className="sighting-img" src={sighting?.image_url} alt="sighting-img"></img>
+      </Link>
     </div>
+  ))}
+</div>
+
+
+
   )
 }
 
