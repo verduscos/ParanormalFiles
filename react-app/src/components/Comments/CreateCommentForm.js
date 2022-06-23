8abd9b2f8d7ffa544923c765d3a0157dc2c5001e
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-06-23 12:33:17 -0700

    added button to display the comment form

--- react-app/src/components/Comments/CreateCommentForm.js
+++ react-app/src/components/Comments/CreateCommentForm.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { createAComment } from "../../store/comment";
-import "../Comments/Comments.css"
+import { BiUserCircle } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
+import "./Form.css";
 
 
 
@@ -13,6 +15,7 @@ const CreateCommentForm = () => {
   const dispatch = useDispatch();
   const [errors, setErrors] = useState([])
   const [comment, setComment] = useState("");
+  const [displayForm, setDisplayForm] = useState(false);
 
 
   const createComment = async (e) => {
@@ -34,30 +37,38 @@ const CreateCommentForm = () => {
   return (
     <>
       {currentUser ?
-        <form id="comment-form-container" onSubmit={createComment}>
-          {errors?.map(error => (
-            <p>{error.split(":")[1]}</p>
-          ))}
-          <textarea
-            className="comment-textarea"
-            value={comment}
-            onChange={(e) => {
-              setComment(e.target.value)
-            }}
-          ></textarea>
-          <div >
-            <button
-              className="comment-btns-edit"
-              onClick={(e) => {
-                e.preventDefault()
-                setComment("")
-              }}>CANCEL</button>
-            <button className="comment-btns-edit">COMMENT</button>
-          </div>
-        </form>
-        :
-        null
-      }
+        <>
+          {displayForm ?
+            <form id="comment-form-container" onSubmit={createComment}>
+              {errors?.map(error => (
+                <p>{error.split(":")[1]}</p>
+              ))}
+              <textarea
+                id="comment-textarea"
+                value={comment}
+                onChange={(e) => {
+                  setComment(e.target.value)
+                }}
+              ></textarea>
+              <div >
+                <button
+                  className="black-btn"
+                  onClick={(e) => {
+                    e.preventDefault()
+                    setDisplayForm(!displayForm)
+                  }}>CANCEL</button>
+                <button className="black-btn">COMMENT</button>
+              </div>
+            </form>
+            :
+            <div id="temp">
+              <BiUserCircle />
+              <button onClick={() => setDisplayForm(!displayForm)}>Add a comment...</button>
+            </div>
+          }
+        </>
+
+        : null}
     </>
   )
 }
