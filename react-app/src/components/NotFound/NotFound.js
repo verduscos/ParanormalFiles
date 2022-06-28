419ecbf6b06e97b69d29efdd0e3e14036ddaec47
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-06-28 00:38:30 -0700

    updating 404 page

--- react-app/src/components/NotFound/NotFound.js
+++ react-app/src/components/NotFound/NotFound.js
@@ -1,19 +1,22 @@
 import React from "react";
-import "./NotFound.css"
 import { Link } from "react-router-dom";
+import { GiSpaceSuit } from "react-icons/gi";
+import "./NotFound.css"
 
 const NotFound = () => {
   return (
-    <div id="notfound-container">
-      <div id="notfound-inner">
-        <div id="notfound-text">
-          <h1>No Results Found</h1>
-          <h2>You shouldn't be here... <Link id="home" to="/">go back.</Link></h2>
-        </div>
-        <div id="img-container">
-          <img id="notfound-img" src="https://medium-clone-photo-bucket.s3.us-west-1.amazonaws.com/stefano-pollio-ZC0EbdLC8G0-unsplash.jpg" alt="not-found-bg" />
-        </div>
-      </div>
+    <div id="lost-container">
+      <ul>
+        <li>
+          <h1 id="lost-header">404</h1>
+        </li>
+        <li>
+          <h2 id="lost-link">There's nothing here... <Link id="home" to="/">go home.</Link></h2>
+        </li>
+        <li>
+          <img src="https://i.gifer.com/7VE.gif" />
+        </li>
+      </ul>
     </div>
   )
 }
