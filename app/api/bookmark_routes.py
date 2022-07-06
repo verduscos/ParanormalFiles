452177a22c2c4274bc3b7ee3c95b638c7a963be0
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-07-05 23:29:27 -0700

    updated bookmark_routes

--- app/api/bookmark_routes.py
+++ app/api/bookmark_routes.py
@@ -11,45 +11,44 @@ def get_bookmarks(userId):
   Get all user favorites.
   """
   bookmarks = Bookmark.query.filter(Bookmark.user_id == userId).all()
-  # total_likes = len(likes)
 
   return {"bookmarks": [bookmark.to_dict() for bookmark in bookmarks]}
 
 
-# @like_routes.route("/", methods=["POST"])
-# def post_like():
-#   """
-#   Post a like to a specific sighting.
-#   """
-#   form = LikeForm()
-#   form["csrf_token"].data = request.cookies["csrf_token"]
-#   if form.validate_on_submit():
-#     searchExists = Like.query.filter(Like.user_id == request.json["user_id"], Like.sighting_id == request.json["sighting_id"]).first()
-#     if searchExists is None:
-#       like = Like(
-#         user_id=request.json["user_id"],
-#         sighting_id=request.json["sighting_id"]
-#       )
-#       db.session.add(like)
-#       db.session.commit()
+@bookmark_routes.route("/", methods=["POST"])
+def post_bookmark():
+  """
+  Post a bookmark to a specific sighting.
+  """
+  form = BookmarkForm()
+  form["csrf_token"].data = request.cookies["csrf_token"]
+  if form.validate_on_submit():
+    searchExists = Bookmark.query.filter(Bookmark.user_id == request.json["user_id"], Bookmark.sighting_id == request.json["sighting_id"]).first()
+    if searchExists is None:
+      bookmark = Bookmark(
+        user_id=request.json["user_id"],
+        sighting_id=request.json["sighting_id"]
+      )
+      db.session.add(bookmark)
+      db.session.commit()
 
-#       return { "likes" : like.to_dict() }
+      return { "bookmarks" : bookmark.to_dict() }
 
-#   return { "likes" : "post to likes failed." }
+  return { "bookmarks" : "post to bookmarks failed." }
 
 
-# @like_routes.route("/", methods=["DELETE"])
-# def delete_like():
-#   """
-#   Delete a like for a specific sighting.
-#   """
-#   form = LikeForm()
-#   # form["csrf_token"].data = request.cookies["csrf_token"]
-#   # if form.validate_on_submit():
-#   searchExists = Like.query.filter(Like.user_id == request.json["user_id"], Like.sighting_id == request.json["sighting_id"]).first()
-#   if searchExists is not None:
-#     db.session.delete(searchExists)
-#     db.session.commit()
-#     return { "deleted" : searchExists.sighting_id }
+@bookmark_routes.route("/", methods=["DELETE"])
+def delete_bookmark():
+  """
+  Delete a bookmark for a specific sighting.
+  """
+  form = Bookmark()
+  # form["csrf_token"].data = request.cookies["csrf_token"]
+  # if form.validate_on_submit():
+  searchExists = Bookmark.query.filter(Bookmark.user_id == request.json["user_id"], Bookmark.sighting_id == request.json["sighting_id"]).first()
+  if searchExists is not None:
+    db.session.delete(searchExists)
+    db.session.commit()
+    return { "deleted" : searchExists.sighting_id }
 
-#   return { "likes" : "delete to likes failed." }
+  return { "bookmarks" : "bookmark delete failed." }
