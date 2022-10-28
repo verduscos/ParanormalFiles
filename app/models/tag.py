847dd9921a8300a6507f28192247d06592f2d057
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-10-28 14:06:20 -0700

    created model for tags table

---
+++ app/models/tag.py
@@ -0,0 +1,14 @@
+from .db import db
+
+class Tag(db.model):
+  __tablename__ = "tags"
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(25), nullable=False)
+
+
+
+  def to_dict(self):
+    return {
+        "id": self.id,
+        "title": self.title,
+    }
