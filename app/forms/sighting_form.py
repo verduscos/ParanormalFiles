439457d0a846ecbc4c0247e89d5efc25cdde8f97
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-11-16 12:50:34 -0800

    removed sighting image requirement from SightingForm

--- app/forms/sighting_form.py
+++ app/forms/sighting_form.py
@@ -18,9 +18,3 @@ class SightingForm(FlaskForm):
             Length(min=5, max=3000, message="Description must be between 5-3000 characters.")
         ]
     )
-    # image_url = StringField(
-    #   "Image",
-    #   validators=[
-    #     DataRequired("Please upload an image.")
-    #   ]
-    # )
