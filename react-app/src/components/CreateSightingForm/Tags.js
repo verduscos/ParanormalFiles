43539809b5d182f77b2ab4fb53523c3e226d2bcf
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-11-17 00:46:35 -0800

    added error handling for tag length

--- react-app/src/components/CreateSightingForm/Tags.js
+++ react-app/src/components/CreateSightingForm/Tags.js
@@ -1,9 +1,13 @@
+import React, { useEffect, useState } from "react";
 import { AiOutlineClose } from 'react-icons/ai';
 import { TiDeleteOutline } from "react-icons/ti";
 
 
 const Tags = (props) => {
+  const [tagErrorMsg, setTagErrorMsg] = useState(null);
   const regex = /^[a-z]+(\s[a-z]+)?$/i
+
+
 
   return (
     <div id="form-category-image-container">
@@ -22,6 +26,7 @@ const Tags = (props) => {
           placeholder="Add a tag..."
           onKeyPress={(e) => {
             if (e.key === 'Enter') {
+              if (e.target.value.length < 2) return setTagErrorMsg("Tags must be at least two characters.")
               if (regex.test(e.target.value) && !props.tags.includes(e.target.value.toLowerCase()) && props.tags.length < 5) {
                 props.setTags(current => [...current, e.target.value.toLowerCase()]);
               }
@@ -33,6 +38,7 @@ const Tags = (props) => {
         />
 
         <p id="tag-instruc"><i>Press enter to add tag.</i></p>
+        {tagErrorMsg ? <p id="tag-instruc" className="error-mssg"><i>{tagErrorMsg}</i></p> : null}
 
         <ul id="tag-container">
           {props.tags?.map((tag, index) => (
