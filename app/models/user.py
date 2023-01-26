c5aa6dcb2eda6c28a82b27a0684396ef03e0201d
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2023-01-26 13:00:10 -0800

    added missing imports for render

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,7 @@
 from turtle import back
-from .db import db
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-
 
 class User(db.Model, UserMixin):
     __tablename__ = "users"
